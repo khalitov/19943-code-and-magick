a5cf600939c31ea74c7f7a7da3eec32ff0260fdd
Nail Khalitov <khalitovn@gmail.com>
2016-05-26 19:00:51 +0300

    Исправлена проверка на массив

--- build/js/check.js
+++ build/js/check.js
@@ -5,49 +5,21 @@ function getMessage(a, b) {
 
     case "number":
       return "Я прыгнул на " + a * 100 + " сантиметров";
-
-    case "number":
-      return "Я прыгнул на " + a * 100 + " сантиметров";
-
-    case "object":
-      {
-        if (typeof b === "object") {
-          var length = 0;
-          for (var i = 0; i < a.length; i++) {
-            length += a[i] * b[i];
-          }
-          return "Я прошёл " + length + " шагов";
-        }
-
-        var sum = 0;
-        for (var i = 0; i < a.length; i++) {
-          sum += a[i];
-        }
-        return "Я прошёл " + sum + " шагов";
+  }
+  if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      var length = 0;
+      for (var i = 0; i < a.length; i++) {
+        length += a[i] * b[i];
       }
+      return "Я прошёл " + length + " шагов";
+    }
+
+    var sum = 0;
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+    return "Я прошёл " + sum + " шагов";
   }
 
-  // if (typeof a === "boolean") {
-  //   return a ? ("Я попал в " + b) : ("Я никуда не попал");
-  // };
-  //
-  // if (typeof a === "number") {
-  //   return "Я прыгнул на " + a * 100 + " сантиметров";
-  // };
-  //
-  // if ((typeof a === "object") && (typeof b !== "object")) {
-  //   var sum = 0;
-  //   for (var i = 0; i < a.length; i++) {
-  //     sum += a[i];
-  //   }
-  //   return "Я прошёл " + sum + " шагов";
-  // };
-  //
-  // if ((typeof a === "object") && (typeof b === "object")) {
-  //   var length = 0;
-  //   for (var i = 0; i < a.length; i++) {
-  //     length += a[i] * b[i];
-  //   }
-  //   return "Я прошёл " + length + " шагов";
-  // }
 }
