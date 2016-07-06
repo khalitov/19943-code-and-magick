aeab49e4ced0cdc7e763251dc875552b257f8e3d
Nail Khalitov <khalitovn@gmail.com>
2016-07-06 18:32:53 +0300

    Новый модуль для констант

--- src/Filter/filter-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  'ALL': 'reviews-all',
-  'RECENT': 'reviews-recent',
-  'POPULAR': 'reviews-popular',
-  'GOOD': 'reviews-good',
-  'BAD': 'reviews-bad'
-};
---
+++ src/consts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  Filter: {
+    'ALL': 'reviews-all',
+    'RECENT': 'reviews-recent',
+    'POPULAR': 'reviews-popular',
+    'GOOD': 'reviews-good',
+    'BAD': 'reviews-bad'
+  },
+  MILLISECONDS_IN_DAY: 1000 * 60 * 60 * 24,
+  MARK_TO_BE_GOOD: 3,
+  REVIEWS_LIST: '//o0.github.io/assets/json/reviews.json',
+  reviewBlock: document.querySelector('.reviews'),
+  LOWEST_POSITIVE_MARK: 3,
+  STATUS_NON_DISPLAY: 'none',
+  STATUS_DISPLAY: 'inline-block',
+};
